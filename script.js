674bc0141dac464f7fb8ba2b75ca287ffc2135e7
Alex2234 <shyrik223@mail.ru>
2023-01-21 17:52:47 +0300

    full block social-project

--- script.js
+++ script.js
@@ -1,72 +1,50 @@
 const slider = document.querySelector('#slider');
 const sliderItems = Array.from(slider.children);
-const btnForward = document.querySelector(".social-project__arrow-forward");
-const btnBack = document.querySelector(".social-project__arrow-back");
+const btnForward = document.querySelectorAll(".slider-button_direction_forward");
+const btnBack = document.querySelectorAll(".slider-button_direction_back");
 
 //Слайдер
-
 sliderItems.forEach(function (slide, index) {
 	// Скрываем все слайды, кроме первого
-	if (index !== 0) slide.classList.add('social-project__slider-item_display-none');
-   
+	if (index !== 0) slide.classList.add('slider-display_none');
 // добавляем индекс
 	slide.dataset.index = index;
-
 	sliderItems[0].setAttribute('data-active', '')
 
-	slide.addEventListener('click', function() {
-		slide.classList.add('social-project__slider-item_display-none');
-		slide.removeAttribute('data-active');
-
-		let nextSlideIndex;
-		if (index +1 === sliderItems.length) {
-			nextSlideIndex=0;
-		} else {
-			nextSlideIndex = index +1;
-		}
-
-		const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-		nextSlide.classList.remove('social-project__slider-item_display-none');
-		nextSlide.setAttribute('data-active', '');
-	});
 });
 
-btnForward.onclick = function () {
-	console.log('nextSlide');
-	const currentSlide = slider.querySelector('[data-active]');
-	const currentSlideIndex = +currentSlide.dataset.index;
-	console.log(currentSlideIndex);
-
-	let nextSlideIndex;
-		if (currentSlideIndex +1 === sliderItems.length) {
-			nextSlideIndex=0;
-		} else {
-			nextSlideIndex = currentSlideIndex +1;
-		}
- 
-		const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-
-		nextSlide.classList.remove('social-project__slider-item_display-none');
-		nextSlide.setAttribute('data-active', '');
-
-}
-
-btnBack.onclick = function () {
-	console.log('nextSlide');
-	const currentSlide = slider.querySelector('[data-active]');
-	const currentSlideIndex = +currentSlide.dataset.index;
-	console.log(currentSlideIndex);
-
-	let nextSlideIndex;
-		if (currentSlideIndex  === 0) {
-			sliderItems.length - 1
-		} else {
-			nextSlideIndex = currentSlideIndex - 1;
-		}
- 
-		const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-
-		nextSlide.classList.remove('social-project__slider-item_display-none');
-		nextSlide.setAttribute('data-active', '');
+// кнопка вперед
+btnForward.forEach(function(buttons) {
+	buttons.onclick = function () {
+	showNextSlide('next');
 	}
+});
 
+
+// енопка назад
+btnBack.forEach(function(buttons) {
+	buttons.onclick = function () {
+		showNextSlide('prev');
+	}
+});
+
+//функция действий со слайдами
+function showNextSlide(direction) {
+	// Скрываем текущий слайд
+	const currentSlide = slider.querySelector('[data-active]');
+	const currentSlideIndex = +currentSlide.dataset.index;
+	currentSlide.classList.add('slider-display_none');
+	currentSlide.removeAttribute('data-active');
+
+	// Рассчитываем след индекс в зависимости от направления движения
+	let nextSlideIndex;
+	if (direction === 'next') {
+		nextSlideIndex = currentSlideIndex + 1 === sliderItems.length ? 0 : currentSlideIndex + 1;
+	} else if (direction === 'prev') {
+		nextSlideIndex = currentSlideIndex === 0 ? sliderItems.length - 1 : currentSlideIndex - 1;
+	}
+	// Показываем след слайд
+	const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+	nextSlide.classList.remove('slider-display_none');
+	nextSlide.setAttribute('data-active', '');
+};
