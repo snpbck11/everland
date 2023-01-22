7318d3e2c4c7731f326c6463261d6ead06c99519
Dasha Kholobaeva <dashaholobaeva@gmail.com>
2023-01-22 23:14:10 +0200

    fix: main-in-work new verion  with accordio completed

--- script.js
+++ script.js
@@ -72,3 +72,45 @@ btnArrow.forEach(function(button) {
 		openMobileMenu(menuMob);
 	})
 });
+
+
+
+// СЕКЦИЯ MAIN-IN-WORK //
+
+// Поворачивает кнопку при нажатии на кнопку
+
+document.querySelectorAll(".main-in-work__button").forEach((item) =>
+item.addEventListener("click", ({ target }) => {
+	target
+		.closest(".main-in-work__box")
+		.querySelectorAll(".main-in-work__button")
+		.forEach((item) => {
+			item.classList.toggle("main-in-work__button_active");
+		});
+	target
+		.closest(".main-in-work__box")
+		.querySelectorAll(".main-in-work__button")
+		.forEach(function (item) {
+			item.classList.toggle("main-in-work__button_active");
+		});
+})
+);
+
+// Открывает аккордеон и поворачивает кнопку при открытии аккордеона
+
+document.querySelectorAll(".main-in-work__wrapper").forEach((item) =>
+item.addEventListener("click", ({ target }) => {
+	target
+		.closest(".main-in-work__box")
+		.querySelectorAll(".main-in-work__button")
+		.forEach((item) => {
+			item.classList.toggle("main-in-work__button_active");
+		});
+	target
+		.closest(".main-in-work__box")
+		.querySelectorAll(".main-in-work__description")
+		.forEach(function (item) {
+			item.classList.toggle("main-in-work__description_active");
+		});
+})
+);
